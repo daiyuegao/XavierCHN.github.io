b5007440c14d2b9578a39a9fb0beda6d75595b82
Xavier Tseng <364671459@qq.com>
2014-09-04 11:19:52 +0800

    面向对象编程

---
+++ OOP.lua
@@ -0,0 +1,28 @@
+ClassTest = {
+	-- constructed function
+	new = function (self, hero)
+		local o_meta = getmetatable( hero )
+		local my_meta = {
+			__index = function( _, name)
+			return self[name] or o_meta.__index[name]
+			end,
+		}
+		setmetatable(hero,my_meta)
+	end,
+
+	-- private variable
+	local _arg1 = 0,
+  
+  -- global variable
+  arg2 = 0,
+  
+	-- getter and setter
+	Arg1 = function( self, o )
+		_arg1 = o or arg1
+		return _arg1
+	end,
+
+	-- class methods
+	func1 = function(self, ...)
+	end,
+}
