f18e4180641b9e98d024dc623d85fff716dd1bba
Xavier Tseng <364671459@qq.com>
2014-09-04 20:50:34 +0800

    123

--- OOP.lua
+++ OOP.lua
@@ -1,10 +1,12 @@
 ClassTest = {
-	-- constructed function
+	-- construcor method
 	new = function (self, hero)
 		local o_meta = getmetatable( hero )
+		-- local o_hero = hero
 		local my_meta = {
-			__index = function( _, name, ... )
-			return self[name](...) or o_meta.__index[name](...)
+			__index = function( _, name )
+				return self[name] or o_meta.__index[name]
+				-- return self[name] or o_hero[name]
 			end,
 		}
 		setmetatable(hero,my_meta)
@@ -22,7 +24,21 @@ ClassTest = {
 		return _arg1
 	end,
 
-	-- class methods
-	func1 = function(self, ...)
+	IsInTheBag = function( self )
+		local mTrigger = Entities:FindByName(nil, "trigger_bag" )
+		if self:IsTouching( mTrigger ) then
+			return true
+		end
+		return false
 	end,
 }
+
+ClassTest:new(hero)
+hero:SetContextThink(DoUniqueString('getting_gold_in_the_bag'),
+	function()
+		if hero:IsInTheBag() then
+			hero:ModifyGold(hero:GetPlayerID(), hero:GetGold() + 20, true, 0)
+		end
+		return 1
+	end,
+1)
